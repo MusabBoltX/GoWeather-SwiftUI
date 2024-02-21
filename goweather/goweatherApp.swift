b70a19ad87aeaa8ed081041ce19ad70d2eb388f6
MusabBoltX <musabsyed714@gmail.com>
2024-02-21 14:40:25 +0500

    Initial Commit

---
+++ goweather/goweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  goweatherApp.swift
+//  goweather
+//
+//  Created by Syed Musab on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct goweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
